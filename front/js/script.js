6f610ca6e701d91e2b51d1dc9210b281020be110
marwan <mir1_93@hotmail.com>
2023-02-02 18:58:31 +0100

    first commit

---
+++ front/js/script.js
@@ -0,0 +1,15 @@
+fetch('http://localhost:3000/api/products')
+.then(reponse => reponse.json())
+.then ((data) => produit(data))
+
+function produit(data) {
+    //console.log(data)
+    const imageUrl = data[0].imageUrl
+    //console.log("url de l'image", imageUrl)
+    const anchor = document.createElement("a")
+    anchor.href = imageUrl
+    anchor.text = "test"
+    const items = document.querySelector("#items")
+    items.appendChild(anchor) 
+    console.log("nous")
+}
