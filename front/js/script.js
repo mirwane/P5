92922e1d8b67e04da251d14b465ffe39496a2063
marwan <mir1_93@hotmail.com>
2023-02-16 13:20:41 +0100

    acceuil

--- front/js/script.js
+++ front/js/script.js
@@ -1,23 +1,54 @@
 fetch("http://localhost:3000/api/products")
   .then((reponse) => reponse.json())
   .then((data) => ajouterProduit(data));
+
 function ajouterProduit(data) {
-  const id = data[0]._id;
-  // console.log("./product.html?id=", id);
+  data.forEach((iteration) => {
+    const { id, imageUrl, altTxt, name, description } = iteration;
+
+    const ancre = makeAncre(id);
+    const article = document.createElement("article");
+    const image = makeImage(imageUrl, altTxt);
+    const h3 = makeH3(name);
+    const p = makeP(description);
+
+    appendproduit(ancre, article);
+    appendArticle(article, image, h3, p);
+  });
+}
+function makeAncre(id) {
   const ancre = document.createElement("a");
   ancre.href = "./product.html?id=" + id;
-  ancre.text = "lien";
+  return ancre;
+}
+function appendproduit(ancre, article) {
   const produit = document.querySelector("#items");
   produit.appendChild(ancre);
+  ancre.appendChild(article);
+}
+function appendArticle(article, image, h3, p) {
+  article.appendChild(image);
+  article.appendChild(h3);
+  article.appendChild(p);
 }
 
-// article.appendChild(image);
-// article.appendChild(titre);
-// article.appendChild(description);
-// const article = document.createElement("article");
-// const image = document.createElement("img");
-// const titre = document.createElement("h3");
-// const description = document.createElement("p");
+function makeImage(imageUrl, altTxt) {
+  const image = document.createElement("img");
+  image.src = imageUrl;
+  image.alt = altTxt;
+  return image;
+}
+function makeH3(name) {
+  const h3 = document.createElement("h3");
+  h3.textContent = name;
+  h3.classList.add("productName");
+  return h3;
+}
+function makeP(description) {
+  const p = document.createElement("p");
+  p.textContent = description;
+  return p;
+}
 
 // "colors": [
 // "Blue",
