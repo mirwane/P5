65409b17a650237160fda1d7c77812627918fc8d
marwan <mir1_93@hotmail.com>
2023-02-20 16:04:55 +0100

    produit

--- front/js/product.js
+++ front/js/product.js
@@ -7,11 +7,12 @@ fetch(`http://localhost:3000/api/products/${id}`)
   .then((id) => ajouterInfos(id));
 
 function ajouterInfos(canape) {
-  const { colors, price, imageUrl, altTxt, name, description, _id } = canape;
+  const { colors, price, imageUrl, altTxt, name, description } = canape;
   makeImage(imageUrl, altTxt);
-  makePrix(price);
+  makePrice(price);
   makeName(name);
   makeDescription(description);
+  makeColor(colors);
 }
 function makeImage(imageUrl, altTxt) {
   const image = document.createElement("img");
@@ -24,7 +25,7 @@ function makeName(name) {
   const nom = document.querySelector("#title");
   nom.textContent = name;
 }
-function makePrix(price) {
+function makePrice(price) {
   const prix = document.querySelector("#price");
   prix.textContent = price;
 }
@@ -32,13 +33,12 @@ function makeDescription(description) {
   const p = document.querySelector("#description");
   p.textContent = description;
 }
-
-//  <p id="description">
-//                   <!-- Dis enim malesuada risus sapien gravida nulla nisl arcu. -->
-//                 </p>
-
-/* <select name="color-select" id="colors">
-                    <option value="">--SVP, choisissez une couleur --</option>
-                    <!--                       <option value="vert">vert</option>
-                      <option value="blanc">blanc</option> -->
-                  </select> */
+function makeColor(colors) {
+  const select = document.querySelector("#colors");
+  colors.forEach((couleurs) => {
+    const option = document.createElement("option");
+    option.value = couleurs;
+    option.textContent = couleurs;
+    select.appendChild(option);
+  });
+}
