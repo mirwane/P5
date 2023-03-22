16e2ff52c8f5ccb93cd10db27454ebda212ccf2f
marwan <mir1_93@hotmail.com>
2023-03-22 14:44:33 +0100

    22/03/2023

--- front/js/product.js
+++ front/js/product.js
@@ -1,14 +1,16 @@
-// Description: Script pour la page produit
+// Script pour la page produit
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const id = urlParams.get("_id");
 let itemPrice = 0;
 let imgUrl, atlText;
-// Description: Récupération des données de l'API
+
+// Récupération des données de l'API
 fetch(`http://localhost:3000/api/products/${id}`)
   .then((reponse) => reponse.json())
   .then((id) => ajouterInfos(id));
-// Description: Création des éléments HTML
+
+// Création des éléments HTML
 function ajouterInfos(canape) {
   const { colors, price, imageUrl, altTxt, name, description } = canape;
   itemPrice = price;
@@ -20,6 +22,8 @@ function ajouterInfos(canape) {
   makeDescription(description);
   makeColor(colors);
 }
+
+// Création image
 function makeImage(imageUrl, altTxt) {
   const image = document.createElement("img");
   image.src = imageUrl;
@@ -27,18 +31,26 @@ function makeImage(imageUrl, altTxt) {
   const item__img = document.querySelector(".item__img");
   item__img.appendChild(image);
 }
+
+// Création nom
 function makeName(name) {
   const nom = document.querySelector("#title");
   nom.textContent = name;
 }
+
+// Création prix
 function makePrice(price) {
   const prix = document.querySelector("#price");
   prix.textContent = price;
 }
+
+// Création description
 function makeDescription(description) {
   const p = document.querySelector("#description");
   p.textContent = description;
 }
+
+// Création couleur
 function makeColor(colors) {
   const select = document.querySelector("#colors");
   colors.forEach((couleurs) => {
@@ -48,7 +60,8 @@ function makeColor(colors) {
     select.appendChild(option);
   });
 }
-// Description: Ajout au panier
+
+// Ajout au panier
 const button = document.querySelector("#addToCart");
 button.addEventListener("click", (e) => {
   const colors = document.querySelector("#colors").value;
@@ -57,7 +70,8 @@ button.addEventListener("click", (e) => {
   panier(colors, quantity);
   redirection();
 });
-// Description: Ajout au local storage
+
+// Ajout au local storage
 function panier(color, quantity) {
   const data = {
     color: color,
@@ -74,14 +88,16 @@ function panier(color, quantity) {
   }
   localStorage.setItem(key, JSON.stringify(data));
 }
-// Description: Vérification des données
+
+// Vérification des données
 function orderInvalide(colors, quantity) {
   if (colors == null || colors === "" || quantity == null || quantity == 0) {
     alert("selectionnez une couleur et une quantité");
     return true;
   }
 }
-// Description: Redirection vers le panier
+
+// Redirection vers le panier
 function redirection() {
   window.location.href = "cart.html";
 }
